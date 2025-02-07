82452a44b051217eb3285f05edc9eb570075df02
MarulBabic <167626236+MarulBabic@users.noreply.github.com>
2025-02-07 15:02:35 +0100

    Add function to store quiz history in localstorage

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 import { fetchQuizData, renderQuestion, lockAnswers } from './quiz.js';
 import { startQuestionTimer, clearQuestionTimer } from './timer.js';
-
+import { saveResult, loadHistory } from './storage.js';
--- storage.js
+++ storage.js
@@ -1,2 +1,17 @@
-
+export function saveResult(resultObj) {
+    let history = JSON.parse(localStorage.getItem('quizHistory')) || [];
+    history.push(resultObj);
+    localStorage.setItem('quizHistory', JSON.stringify(history));
+  }
+  
+  export function loadHistory() {
+    const historyListEl = document.getElementById('history-list');
+    let history = JSON.parse(localStorage.getItem('quizHistory')) || [];
+    historyListEl.innerHTML = '';
+    history.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = `${item.date} - ${item.category} (${item.difficulty}) - ${item.score}/5`;
+      historyListEl.appendChild(li);
+    });
+  }
   
